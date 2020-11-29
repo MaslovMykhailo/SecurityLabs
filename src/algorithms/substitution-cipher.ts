17f21528c3ea6a047213d8fb58e20eedd2aa7d4f
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2020-11-29 22:11:40 +0200

    Introduce poly alphanet substitution cipher

--- src/algorithms/substitution-cipher.ts
+++ src/algorithms/substitution-cipher.ts
@@ -1,8 +1,10 @@
 import {toChars} from '../utils'
 
+type CharSet = string | string[] 
+
 export const createSubstitutionCipherApplier = (
-    alphabet: string | string[],
-    key: string | string[]
+    alphabet: CharSet,
+    key: CharSet
 ) => (text: string) => {
     const alphabetChars = toChars(alphabet)
     const keyChars = toChars(key)
@@ -24,6 +26,29 @@ export const createSubstitutionCipherApplier = (
 
 export const applySubstitutionCipher = (
     text: string,
-    alphabet: string | string[],
-    key: string | string[]
+    alphabet: CharSet,
+    key: CharSet
 ) => createSubstitutionCipherApplier(alphabet, key)(text)
+
+export const createPolyAlphabetSubstitutionCipherApplier = (
+    alphabet: CharSet,
+    keys: CharSet[]
+) => {
+    const cipherAppliers = keys
+        .map(toChars)
+        .map(key => createSubstitutionCipherApplier(alphabet, key))
+    
+    return (text: string) => text
+        .split('')
+        .map((symbol, index) => {
+            const applyCipher = cipherAppliers[index % cipherAppliers.length]
+            return applyCipher(symbol) ?? ''
+        })
+        .join('')
+}
+
+export const applyPolyAlphabetSubstitutionCipher = (
+    text: string,
+    alphabet: CharSet,
+    keys: CharSet[]
+) => createPolyAlphabetSubstitutionCipherApplier(alphabet, keys)(text)
