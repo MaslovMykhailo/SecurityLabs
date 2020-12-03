1c594aa67555d033caafed714f32d34d98b1afa1
Mykhailo Maslov <maslov.mikhail2000@gmail.com>
2020-12-03 21:19:43 +0200

    Solve task3 for lab3

--- src/lab3/index.ts
+++ src/lab3/index.ts
@@ -1,9 +1,11 @@
 import {task1} from './task1'
 import {task2} from './task2'
+import {task3} from './task3'
 
 export const lab3 = async () => {
     await Promise.all([
         task1(),
-        task2()
+        task2(),
+        task3()
     ])
 }
---
+++ src/lab3/task3/index.ts
@@ -0,0 +1,34 @@
+import path from 'path'
+
+import {MersenneTwister} from '../../algorithms'
+import {writeFile} from '../../utils'
+import {collectionDesiredAmountOfMoney, OnlineCasino, PlayMode} from '../common'
+
+export const task3 = async () => {
+    let amountOfMoney = 0
+
+    const {id: accountId} = await OnlineCasino.createAccount()
+
+    const mtStates: number[] = []
+    while (mtStates.length < MersenneTwister.N) {
+        const {realNumber, account: {money}} = await OnlineCasino.makeBet({
+            accountId,
+            mode: PlayMode.BetterMt,
+            money: 1,
+            number: Math.round(Math.random() * 100)
+        })
+        mtStates.push(realNumber)
+        amountOfMoney = money
+    }
+
+    const mt = MersenneTwister.fromMTStates(mtStates)
+
+    const solution = await collectionDesiredAmountOfMoney({
+        accountId,
+        mode: PlayMode.BetterMt,
+        initialAmountOfMoney: amountOfMoney,
+        getNextNumber: () => mt.next()
+    })
+
+    await writeFile(path.join(__dirname, 'solution.txt'), solution)
+}
